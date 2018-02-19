75040ae7f380f40464d8999ef7c40f157b78e615
Ben Weedon <ben.weedon@outlook.com>
2018-02-18 17:16:30 -0800

    Add clap command line options
    Also update Cargo.toml with more information.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+#[macro_use]
+extern crate clap;
+
+use clap::App;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    App::from_yaml(yaml)
+        .name(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .get_matches();
 }
